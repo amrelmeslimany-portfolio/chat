2b7a39f48b5382074e1743fa95e56d9922e186c1
amrelmeslimany-portfolio <aamr_786@hotmail.com>
2023-08-26 13:20:59 +0300

    Update API Server

--- src/constants/words.js
+++ src/constants/words.js
@@ -8,4 +8,7 @@ export const DATE_FORMAT = "dd/MM/yyyy";
 
 export const IMG_HOSTING = "https://res.cloudinary.com";
 
-export const API_SERVER = "http://127.0.0.1:3005";
+export const API_SERVER =
+  process.env.NODE_ENV === "production"
+    ? "https://amr-chat.onrender.com"
+    : "http://127.0.0.1:3005"; // In Production
